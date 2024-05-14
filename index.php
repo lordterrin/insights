fb7a8c9770d22a033fcf6d8e5a6afd1ceb04f8aa
BP <lordterrin@gmail.com>
2024-05-13 19:46:04 -0700

    2

--- index.php
+++ index.php
@@ -4,4 +4,56 @@
 <?php include 'header.php';?>
 <?php include 'footer.php';?>
 
+<body>
+
+<div class="header">
+
+  <div class="top_header">
+    <div class="left_header">
+      <div class="left_header_title">Ensign Insights</div>
+      <div class="left_header_subtitle"><img class="ensights" src="img/ensights.png" /></div>
+    </div>
+    <div class="right_header">
+      <div class="left_header_item"></div>
+    </div>
+  </div>
+
+  <div class="subheader">
+    <div id="home" class="right_header_item">
+      <div class="header_image"><img src="img/home.png"></div>
+      <div class="header_subtitle">Insight 1</div>
+    </div>
+    <div id="modes" class="right_header_item">
+      <div class="header_image"><img src="img/modes.png"></div>
+      <div class="header_subtitle">Insight 2</div>
+    </div>
+    <div id="builders" class="right_header_item">
+      <div class="header_image"><img src="img/prophet.png"></div>
+      <div class="header_subtitle">Insight 3</div>
+    </div>
+  </div>
+</div>
+
+<div class="body">
+</div>
+
+<div class="legion_footer">
+
+    <div class="th_footer_filter hideme">
+      <div title="search by literally any word anywhere" id="units_table_filter_alt" class="dataTables_filter_alt">
+        <label>
+          <div class="filter_text_alt">Filter</div>
+          <input id="th_filter" type="search">
+        </label>
+      </div>
+    </div>
+
+    <div class="footer_item"></div>
+    <div class="footer_item" onclick="about_swal()">About</div>
+    <div class="footer_item">Home</div>
+  </div>
+
+
+</body>
+
 
